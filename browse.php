d8edf891293a528c49a3cef746266595f57e6552
Todori <113155294+fevertodori@users.noreply.github.com>
2023-02-10 12:31:34 +0900

    追加

---
+++ browse.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>閲覧ページ</title>
+    <link href="browse.css" rel="stylesheet">
+</head>
+<body>
+    <h1>title</h1>
+    <button onclick="location.href='./#'">編集</button>
+    <img src="img/gogo1.png" alt="">
+    <h2>タイトル</h2>
+    <h3>投稿者名</h3>
+    <p>内容</p>
+    <button onclick="location.href='./#'">戻る</button>
+    <a href="#">前へ</a>
+    <a href="#">次へ</a>
+</body>
+</html>
